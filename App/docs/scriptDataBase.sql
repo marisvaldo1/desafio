4c4f144309398aa03d718bb7ccc2eefca849d3b3
Marisvaldo <marisvaldo@gmail.com>
2022-07-19 10:44:07 -0300

    Ajustes na criação do banco

--- App/docs/scriptDataBase.sql
+++ App/docs/scriptDataBase.sql
@@ -1,39 +1,66 @@
-CREATE DATABASE `desafio` /*!40100 COLLATE 'utf8mb4_general_ci' */
+-- --------------------------------------------------------
+-- Servidor:                     localhost
+-- Versão do servidor:           10.4.19-MariaDB - mariadb.org binary distribution
+-- OS do Servidor:               Win64
+-- HeidiSQL Versão:              12.0.0.6468
+-- --------------------------------------------------------
 
-CREATE TABLE `products` (
-	`id_product` INT(11) NOT NULL AUTO_INCREMENT,
-	`name` VARCHAR(255) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`sku` VARCHAR(255) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`price` FLOAT NOT NULL,
-	`description` TEXT NOT NULL COLLATE 'utf8mb4_general_ci',
-	`quantity` INT(11) NOT NULL,
-	`image` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
-	PRIMARY KEY (`id_product`) USING BTREE
-)
-COLLATE='utf8mb4_general_ci'
-ENGINE=InnoDB
-AUTO_INCREMENT=1
-;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
-CREATE TABLE `products_category` (
-	`id_product` INT(11) NULL DEFAULT NULL,
-	`id_category` INT(11) NULL DEFAULT NULL,
-	INDEX `FK__products` (`id_product`) USING BTREE,
-	INDEX `FK__categories` (`id_category`) USING BTREE,
-	CONSTRAINT `FK__categories` FOREIGN KEY (`id_category`) REFERENCES `categories` (`id_category`) ON UPDATE NO ACTION ON DELETE NO ACTION,
-	CONSTRAINT `FK__products` FOREIGN KEY (`id_product`) REFERENCES `products` (`id_product`) ON UPDATE NO ACTION ON DELETE NO ACTION
-)
-COLLATE='utf8mb4_general_ci'
-ENGINE=InnoDB
-;
+-- Copiando estrutura do banco de dados para desafio
+DROP DATABASE IF EXISTS `desafio`;
+CREATE DATABASE IF NOT EXISTS `desafio` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `desafio`;
 
-CREATE TABLE `categories` (
-	`id_category` INT(11) NOT NULL AUTO_INCREMENT,
-	`name` VARCHAR(255) NOT NULL,
-	`code` VARCHAR(50) NOT NULL,
-	PRIMARY KEY (`id_category`) USING BTREE
-)
-COLLATE='utf8mb4_general_ci'
-ENGINE=InnoDB
-;
+-- Copiando estrutura para tabela desafio.categories
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id_category` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `code` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_category`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4;
+
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela desafio.products
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE IF NOT EXISTS `products` (
+  `id_product` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `sku` varchar(255) NOT NULL,
+  `price` float NOT NULL,
+  `description` text NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_product`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela desafio.products_category
+DROP TABLE IF EXISTS `products_category`;
+CREATE TABLE IF NOT EXISTS `products_category` (
+  `id_product` int(11) DEFAULT NULL,
+  `id_category` int(11) DEFAULT NULL,
+  KEY `FK__products` (`id_product`) USING BTREE,
+  KEY `FK__categories` (`id_category`) USING BTREE,
+  CONSTRAINT `FK__categories` FOREIGN KEY (`id_category`) REFERENCES `categories` (`id_category`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK__products` FOREIGN KEY (`id_product`) REFERENCES `products` (`id_product`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Exportação de dados foi desmarcado.
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
